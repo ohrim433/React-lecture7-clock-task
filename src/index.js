8e54902f99b35ff4d132c946f4d82d5924274643
ohrim433 <57233121+ohrim433@users.noreply.github.com>
2020-03-11 11:48:26 +0200

    Inital Commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {timestamp: new Date};
+
+        this.timerId = setInterval(() => this.setState({timestamp: new Date}), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    render() {
+        const {timestamp} = this.state;
+
+        return (
+            <div>
+                {timestamp.toLocaleTimeString()}
+            </div>
+        );
+    }
+}
+
+function render() {
+    ReactDOM.render(<App/>, document.getElementById('root'));
+}
+
+render();
+
+export default render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
